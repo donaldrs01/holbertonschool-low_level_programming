95fd19d2e17e6bacfe313cbcf648a5be27043ed4
Your Name <you@example.com>
2023-09-22 16:46:14 -0700

    printf %zu bytes \n followed by sizeof(char) or (int) etc.

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -6,7 +6,7 @@
  * Return: value should return as 0
  */
 int main(void)
-
+{
 	printf("Size of char: %zu bytes\n", sizeof(char));
 	printf("Size of int: %zu bytes\n", sizeof(int));
 	printf("Size of long: %zu bytes\n", sizeof(long));
