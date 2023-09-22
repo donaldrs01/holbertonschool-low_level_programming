1f2bff0ab7d146442a9394520e2d921af91df478
Your Name <you@example.com>
2023-09-22 16:44:33 -0700

    printf %zu bytes \n followed by sizeofsizeof(char) or (int) etc.

---
+++ hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - The main function
+ *
+ * Description: this will print the size of various types
+ * Return: value should return as 0
+ */
+int main(void)
+
+	printf("Size of char: %zu bytes\n", sizeof(char));
+	printf("Size of int: %zu bytes\n", sizeof(int));
+	printf("Size of long: %zu bytes\n", sizeof(long));
+	printf("Size of long long: %zu bytes\n", sizeof(long long));
+	printf("Size of float: %zu bytes\n", sizeof(float));
+	return (0);
+}
