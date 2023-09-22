f85d148b770b23b8e0e53ec45c6c2afbc7576fb7
Your Name <you@example.com>
2023-09-22 14:57:21 -0700

    closely follow betty guidelines! or you will go crazy

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 /**
- * main - This is a description
+ * main - This is a description of the main function.
+ *
+ * Description: it should print a line of text.
+ * Return: value should be 0 (indicates success).
  */
-void main(void)
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
