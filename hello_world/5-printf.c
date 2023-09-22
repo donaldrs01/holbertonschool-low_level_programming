f0ab8b350fb8d472b50e69c36aec54199b4e8718
Your Name <you@example.com>
2023-09-22 15:06:04 -0700

    printf similar to puts

---
+++ hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - This is a description of the main function.
+ *
+ * Description: it should print a line of text.
+ * Return: value should be 0 (indicates success).
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,"); 
+	return (0);
+}
