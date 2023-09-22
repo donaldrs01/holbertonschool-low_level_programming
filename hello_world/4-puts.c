45d128a3d002ca7db935b029ab32c343dcb537cc
Your Name <you@example.com>
2023-09-22 13:49:27 -0700

    formatting sucks

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
-
+/* This should use the puts function to print text. */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\"");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
