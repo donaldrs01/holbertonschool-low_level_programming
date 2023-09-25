b787f7b1e774be24992ca0f61b68f976a01766f5
Your Name <you@example.com>
2023-09-25 16:13:02 -0700

    did this work?

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include <main.h>
+#include <stdio.h>
+/**
+ * print_alphabet - prints out the alphabet in lowercase
+ */
+void print_alphabet(void)
+{
+	char letter = 'a';
+
+	while (letter <= 'z')
+	{
+		_putchar(letter);
+		letter++;
+	}
+	_putchar('\n');
+}
