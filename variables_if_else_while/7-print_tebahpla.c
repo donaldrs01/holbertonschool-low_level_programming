0e99e7681b4f7c399ecc1caa3ace7f00bb2cfde0
Your Name <you@example.com>
2023-09-25 14:33:43 -0700

    use 'letter--' to have letter variable starting at z to move to previous letter

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - this will print the lowercase alphabet in reverse
+ *
+ * Return: it should successfully return a 0.
+ */
+int main(void)
+{
+	char letter = 'z';
+
+	while (letter >= 'a')
+	{
+		putchar(letter);
+		letter--;
+	}
+	putchar('\n');
+}
