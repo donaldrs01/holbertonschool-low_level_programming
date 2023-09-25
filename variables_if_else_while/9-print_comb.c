fdac977046909352bbdcb3981a122ed5abd11a50
Your Name <you@example.com>
2023-09-25 15:04:27 -0700

    putchar for commas and spaces

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -7,18 +7,17 @@
 int main(void)
 {
 	int i = 0;
-	int j = i + 1;
 	
 	for (i = 0; i <= 9; i++)
 	{
-		for (j = i + 1; j <= 9; j++)
+		putchar(i + '0');
+
+		if (i !=9)
 		{
-			putchar(i + '0');
 			putchar(',');
-			putchar(' '); 
-			putchar(j + '0');
+			putchar(' ');
 		}
 	}
-	putchar ('\n');
+	putchar('\n');
 	return (0);
 }
