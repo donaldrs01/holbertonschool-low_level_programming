98518a9aa50e570b24e06f04863535fef8758de3
Your Name <you@example.com>
2023-09-25 16:56:07 -0700

    piping lowercase and uppercase together

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _isalpha - checks if a character is part of the alphabet
+ * @c: the character to check
+ *
+ * Return: 1 if c is part of the alphabet, 0 if it is not. 
+ */
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+		return (1);
+	else
+		return (0);
+}
