0d37832d1ad082be2381d87eddfbd06310cb6451
Your Name <you@example.com>
2023-09-25 15:25:40 -0700

    simple printf statement

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - this program should print _putchar
+ *
+ * Return: this should return a value of 0 when successful.
+ */
+int main(void)
+{
+	printf("_putchar\n"i);
+	return (0);
+}
