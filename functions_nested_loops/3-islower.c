5071f8459271e592fc0cce7a404398bf22107496
Your Name <you@example.com>
2023-09-25 16:50:51 -0700

    if c is between a-z, return 1. else, return 0

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -1,12 +1,15 @@
 #include "main.h"
-#include <stdio.h> 
+#include <stdio.h>
 /**
  * _islower - checks if a character is lowercase
+ * @c - the character to check
+ *
+ * Return: 1 if lowercase, 0 is uppercase
  */
 int _islower(int c)
 {
 	if (c >= 'a' && c <= 'z')
-		return 1;
+		return (1);
 	else
-		return 0;
+		return (0);
 }
