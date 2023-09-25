74c4b6d629d37bf28d6c2051d6e9fca2343ad7a5
Your Name <you@example.com>
2023-09-25 15:53:49 -0700

    guessing still

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -3,5 +3,13 @@
 
 int _putchar(char c)
 {
-	return write(1, &c, 1);
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	return (0);
 }
