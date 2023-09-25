fede6a8933829a1b20e2a8ab66c2608c4e2303b3
Your Name <you@example.com>
2023-09-25 14:14:21 -0700

    printf %d\n, i

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - prints numbers 0-9 in separate lines
+ *
+ * Return: should return 0 upon success.
+ */
+int main(void)
+{
+	int i = 0;
+
+	for (; i <= 9; i++)
+	{
+		printf("%d\n", i);
+	}
+return (0);
+}
