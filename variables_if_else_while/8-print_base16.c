33a319dc94cd21edc2aaa75e8d4d21b40c38c4fe
Your Name <you@example.com>
2023-09-25 14:47:57 -0700

    c iterates through 0-f, use if statement to check if c is hexadecimal

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - this will print all base 16 numbers in lowercase
+ *
+ * Return: it should return the number 0 on success.
+ */
+int main(void)
+{
+
+	for (char c = '0'; c <= 'f'; c++)
+	{
+		if ((c >= '0' && c <= '9') || (c >= 'a' && c <= 'f'))
+		{
+			putchar(c);
+		}
+	}
+	putchar('\n');
+	return (0);
+}	
