2beed9bf7119443827e7aa8aab428cc5fe4fbb05
Your Name <you@example.com>
2023-09-25 17:07:39 -0700

    if n<0, return -n will return positive value of n

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h> 
+/**
+ * _abs - computers the absolute value of integer 'n' 
+ * @n: the integer to compute the absolute value of 
+ *
+ * Return: absolute value of n
+ */
+int _abs(int n)
+{
+	if (n < 0)
+		return -n;
+	else
+		return n;
+}
