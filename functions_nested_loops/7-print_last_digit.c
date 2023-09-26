784652f360275fd8861bb4807a718891e542539b
Your Name <you@example.com>
2023-09-25 17:16:09 -0700

    only return positive val values, then use n % 10 to find last digit

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h> 
+/**
+ * print_last_digit - will print the last digit of a number 
+ * @n - the integer that is being evaluted 
+ *
+ * Return: the last digit
+ */
+int print_last_digit(int n)
+{
+	int last_digit;
+
+	if (n < 0)
+		n = -n;
+	last_digit = n % 10;
+	_putchar('0' + last_digit);
+	return (last_digit);
+}
