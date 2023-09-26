546476208c59ac2056b685c08f78bf51ad7a957a
Your Name <you@example.com>
2023-09-25 18:13:39 -0700

    int add(int a, int b) return a+b)

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * add - adds two integers togetheri
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: the sum of a and b 
+ */
+int add(int a, int b)
+{
+	return a + b;
+}
