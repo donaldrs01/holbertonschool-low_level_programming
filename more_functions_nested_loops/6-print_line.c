f8308dce2f5472e8aae17b66aba292c3f075944d
Your Name <you@example.com>
2023-09-29 13:59:44 -0700

    blah

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_line - should draw a straight line with __ characters
+ * @n: the number of times the character _ is printed 
+ */
+void print_line(int n)
+{
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		int i;
+		for (i = 0; i < n; i++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
+}
