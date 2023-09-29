a21d35d689f38af4314c8faa64d5ace8c57fcace
Your Name <you@example.com>
2023-09-29 12:47:20 -0700

    using result += a; to perform multiplication

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * mul - multiplies two integers together
+ * @a: the first integer
+ * @b: the second integer
+ *
+ * Return: should return 0
+ */
+
+int mul(int a, int b)
+{
+	while (b > 0)
+	{
+		result += a;
+		b--;
+	}
+	return (0)
+}
