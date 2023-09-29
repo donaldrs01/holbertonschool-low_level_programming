787100dcbc44aef3dbb4635af83ac5dfd921e16f
Your Name <you@example.com>
2023-09-29 13:38:17 -0700

    blah

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -10,8 +10,11 @@ void more_numbers(void)
 	for (i = 0; i < 10; i++) /* loops 10 times for each set of numbers */
 	{
 		for (k = 0; k <= 14; k++) /* loop through the numbers 1-14 */
-		{
+			if (j >= 10)
 			{
+				_putchar('0' + (k / 10)); /* print the tens digit */
+			}
+		{
 				_putchar('0' + (k % 10));
 			}
 		}
