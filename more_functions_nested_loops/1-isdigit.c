1c48c3a29e3c1006c592471457b945e83a8c7af0
Your Name <you@example.com>
2023-09-29 12:34:11 -0700

    is number between 1-9

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _isdigit - function that tells us if character is a digit (between 1-9) 
+ * @c - the character 
+ *
+ * Return: 1 if digit, 0 if not
+ */
+int _isdigit(int c);
+{
+	if (c >= '0' && c <= '9')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
