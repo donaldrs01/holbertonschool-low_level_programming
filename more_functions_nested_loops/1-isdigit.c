06c952c81d792abb29efc8154a8d7ca61c80bfd8
Your Name <you@example.com>
2023-09-29 12:37:20 -0700

    is number between 1-9

--- more_functions_nested_loops/1-isdigit.c
+++ more_functions_nested_loops/1-isdigit.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
- * _isdigit - function that tells us if character is a digit (between 1-9) 
- * @c - the character 
+ * _isdigit - function that tells us if character is a digit (between 1-9)
+ * @c: the character
  *
  * Return: 1 if digit, 0 if not
  */
