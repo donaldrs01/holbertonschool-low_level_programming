eeea66cc03d9e58fde46ce8f0cc01277be3c72e6
Your Name <you@example.com>
2023-09-29 13:33:37 -0700

    use k % 10 inner loop to print ones digit

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -5,7 +5,6 @@
  */
 void more_numbers(void)
 {
-	float numbers[] = {0.0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0, 11.0, 12.0, 13.0, 14.0};
 	int i, k;
 
 	for (i = 0; i < 10; i++) /* loops 10 times for each set of numbers */
