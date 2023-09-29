42cdd5b5e22660937fa96229a7a1fb50872187b8
Your Name <you@example.com>
2023-09-29 10:31:55 -0700

    should be good

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -1,6 +1,7 @@
 #include "main.h"
 /**
  * _isupper - tells whether a character is upper or lower case
+ * @c: the character being checked for case
  * Return: 1 on upper case, 0 on lower case
  */
 int _isupper(int c)
