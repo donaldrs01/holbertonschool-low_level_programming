0d664336cb1da81e6f6710d30a0bfbfc34297d85
Your Name <you@example.com>
2023-09-29 12:56:37 -0700

    digit++, _putchar(digit)

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+include "main.h" 
+/**
+ * print_numbers - will print from 0 to 9 
+ *
+ */
+void print_numbers(void)
+{
+	char digit;
+
+	for (digit = '0'; digit <= '9'; digit++)
+	{
+		_putchar(digit);
+	}
+	_putchar('\n');
+}
