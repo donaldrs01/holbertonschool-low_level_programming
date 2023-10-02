ed7c96300fd245439f94b932822d0ab5adeb611b
Your Name <you@example.com>
2023-10-02 12:05:45 -0700

    sets starting index at halfway through or halfway -1, then prints remainder of string

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * puts_half - will print half of the string input
+ * @str: the string input
+ */
+void puts_half(char *str)
+{
+	int length = 0;
+	int start;
+	int i;
+
+	while (str[length] != '\0')
+	{
+		length++;
+	}
+
+	if (length % 2 == 0)
+	{
+		start = length / 2;
+	}
+
+	else
+	{
+		start = (length -1) / 2;
+	}
+
+	for (i = start; i < length; i++)
+	{
+	_putchar(str[i]);
+	}
+}	
