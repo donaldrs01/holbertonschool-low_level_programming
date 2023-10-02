94981f1f6f1e6fe89d57bb335786de451c58d7cc
Your Name <you@example.com>
2023-10-02 09:48:07 -0700

    triangle

---
+++ more_functions_nested_loops/10-print_triangles.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * print_trinagle - will use # character to print triangle based on variable 'size' 
+ * @size: the size of the desired triangle
+ */
+
+void print_triangle(int size);
+{
+	int row, space, hash; 
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+
+	for (row = 1; row <= size; row++)
+	{
+		for (space = 0; space < size - row; space++)
+		{
+			_putchar(' ');
+		}
+		for (hash = 0; hash < row; hash++)
+		{
+			_putchar('#);
+		}
+		_putchar('\n');
+	}
+}
