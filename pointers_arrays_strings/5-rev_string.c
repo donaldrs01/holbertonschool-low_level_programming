0f8ef7f6b44ac00d6a9999a8e5f2a1a346e55840
Your Name <you@example.com>
2023-10-02 11:27:41 -0700

    set two indices at start art and end of string, then swapped characters as each index moved inward

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * rev_string - reverses inputted string 
+ * @s: the string that will be reversed 
+ */
+void rev_string(char *s)
+{
+	int length = 0;
+	int start = 0;
+	int end;
+	char temp;
+
+	while (s[length] != '\0')
+	{
+		length++;
+	}
+
+	end = length - 1;
+
+	while (start < end)
+	{
+		char temp = s[start];
+		s[start] = s[end];
+		s[end] = temp;
+
+		start++;
+		end--;
+	}
+}
