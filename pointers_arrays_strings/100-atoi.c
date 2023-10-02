106a0bb637d9ece06811d96e9e486b51ae34b029
Your Name <you@example.com>
2023-10-02 12:41:23 -0700

    blah

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,48 @@
+#include "main.h"
+/**
+ * _atoi - converts a string to an integer
+ * *s: the pointer to a string
+ *
+ * Return: 0 if no numbers in string, otherwise return the string 
+ */
+int _atoi(char *s)
+{
+	int result = 0;
+	int sign = 1;
+
+	while (*s == ' ' || *s == '\t')
+	{
+		s++
+	}
+
+	if (*s == '-')
+	{
+		sign = -1;
+		s++;
+	}
+
+	else if (*s == '+')
+	{
+		s++;
+	}
+
+	while (*s >= '0' && *s <= '9')
+	{
+		if (result > (INT_MAX - (*s - '0')) / 10)
+		{
+			if (sign == 1)
+			{
+				return INT_MAX;
+			}
+			else
+			{
+				return INT_MIN;
+			}
+		}
+
+		result = result * 10 + (*s - '0');
+		s++;
+	}
+
+	return (result * sign);
+}
