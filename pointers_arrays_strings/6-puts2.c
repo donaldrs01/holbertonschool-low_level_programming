faa9af04c8baa9ef7c9216f309d35e1c89a81b15
Your Name <you@example.com>
2023-10-02 15:59:39 -0700

    didn't end at null terminated

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -7,11 +7,22 @@ void puts2(char *str)
 {
 	int i = 0;
 
-	while (str[i] != '\0')
+	while (str[i] != '\0'i)
 	{
 		_putchar(str[i]);
 		i += 2;
 		/* This is the syntax for going up by 2 */
+
+		if (str[i] != '\0')
+		{
+			_putchar(str[i]);
+			i += 2;
+		}
+		else
+		{
+			break;
+		}
+
 	}
 _putchar ('\n');
 }
