aefd5b9426077399f316162db6c329f91a892264
Your Name <you@example.com>
2023-10-02 10:37:44 -0700

    *n = 98

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * reset_to_98 - takes a pointer to an int and updates its value to 98
- * @*n: the integer whose value will be changed
+ * @n: the integer whose value will be changed
  */
 void reset_to_98(int *n)
 {
