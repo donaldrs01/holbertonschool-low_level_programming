e9432b5bcac05ec7991bb28456b1b6e4304041be
Your Name <you@example.com>
2023-10-02 11:48:36 -0700

    for length of string, i +i += 2 to increment by 2

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * puts2 - prints every other character of a string
+ * @str: the inputted string
+ */
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0/)
+	{
+		putchar(str[i]);
+		i += 2;
+		/* This is the syntax for going up by 2 */
+	}
+putchar ('\n')
+}	
