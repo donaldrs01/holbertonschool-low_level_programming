6f2cd56b7dfc643ce37e9b6fbaedd14a1490db77
Your Name <you@example.com>
2023-10-02 12:16:45 -0700

    for loop to iterate thro through array and print as integers

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_array - will print n elements of an array of integers
+ * @a: the array of integers 
+ * @n: the number of integers that will be printed from list
+ */
+void print_array(int *a, int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		printf("%d", a[i]);
+		if (i < n - 1)
+		{
+			printf(",");
+		}
+	}
+	printf("\n");
+}
