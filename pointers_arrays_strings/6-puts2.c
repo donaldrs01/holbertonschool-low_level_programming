eb6f7701ba558ca151f30f4819a8c461915fe02f
Your Name <you@example.com>
2023-10-02 11:51:08 -0700

    for length of string, i += 2 to increment by 2

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -7,11 +7,11 @@ void puts2(char *str)
 {
 	int i = 0;
 
-	while (str[i] != '\0/)
+	while (str[i] != '\0/');
 	{
 		putchar(str[i]);
 		i += 2;
 		/* This is the syntax for going up by 2 */
 	}
 putchar ('\n')
-}	
+}
