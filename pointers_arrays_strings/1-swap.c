8ebef8b05ccbaa8fdc2e72aef42211610ddd1efa
Your Name <you@example.com>
2023-10-02 10:41:55 -0700

    use a temporary third variable to store value

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * swap_int - will swap the value of two integers
+ * @a: the value of one integer 
+ * @b: the value of the second  integer 
+ */
+void swap_int (int *a, int *b)
+{
+	int temp = *a;
+	*a = *b;
+	*b = temp;
+}
