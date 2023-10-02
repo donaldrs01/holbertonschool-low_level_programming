c7765f01f793a819ea34cce8a12699f1879bc6ff
Your Name <you@example.com>
2023-10-02 10:58:55 -0700

    prints until it reaches the null terminator of the string

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _puts - will print a string
+ * @str: the string that is printed out
+ */
+void _puts(char *str);
+{
+	while (*str != '\0')
+	{
+		putchar(*str);
+		str++;
+	}
+	putchar('\n');
+}
