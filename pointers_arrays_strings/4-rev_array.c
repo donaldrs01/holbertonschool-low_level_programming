c1847454f25a073129b25e546b2b3e71df8f845b
Your Name <you@example.com>
2023-10-05 12:06:48 -0700

    establish start and end end variables, then swap the elements as they move towards each other

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * reverse_array - will reverse input n amount of elements of an array
+ * @n: the number of elements of the array
+ * @a: an array of integers
+ */
+void reverse_array(int *a, int n);
+int start = 0;
+int end = n - 1;
+int temp;
+
+while (start < end)
+{
+	temp = a[start];
+	a[start] = a[end];
+	a[end] = temp;
+
+	start++;
+	end--;
+}
