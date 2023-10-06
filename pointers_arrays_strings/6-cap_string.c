138eea97fdfd2a75901d251e0204ead049c8130f
Your Name <you@example.com>
2023-10-06 09:05:22 -0700

    added each separator as ir as its own prompt for \n

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * *cap_string - will capitalize the first letter of all words in a string
+ * @str: the input string
+ *
+ * Return: the capitalized string
+ */
+char *cap_string(char *str)
+{
+	for (int i = 0; str[i] != '\0'; i++)
+	{
+		if ((i == 0 || str[i - 1] == ' ' || str[i - 1] == '\t' || str[i - 1] == '\n' || str[i - 1] == ',' || str[i - 1] == ';' || str[i - 1] == '.' || str[i - 1] == '!' || str[i - 1] == '?' || str[i - 1] == '"' || str[i - 1] = '(' || str[i - 1] = ')' || str[i - 1] = '{' || str[i - 1] == '}') && (str[i] >= 'a' && str[i] <= 'z'))
+		{
+			str[i] = str[i] - 'a' + 'A';
+		}
+	}
+
+	return (str);
+}
