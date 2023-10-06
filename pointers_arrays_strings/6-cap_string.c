694c6d047663f27c781ff31435dd7960972f4979
Your Name <you@example.com>
2023-10-06 09:07:21 -0700

    blah

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -7,6 +7,8 @@
  */
 char *cap_string(char *str)
 {
+	int i;
+
 	for (int i = 0; str[i] != '\0'; i++)
 	{
 		if ((i == 0 || str[i - 1] == ' ' || str[i - 1] == '\t' || str[i - 1] == '\n' || str[i - 1] == ',' || str[i - 1] == ';' || str[i - 1] == '.' || str[i - 1] == '!' || str[i - 1] == '?' || str[i - 1] == '"' || str[i - 1] = '(' || str[i - 1] = ')' || str[i - 1] = '{' || str[i - 1] == '}') && (str[i] >= 'a' && str[i] <= 'z'))
