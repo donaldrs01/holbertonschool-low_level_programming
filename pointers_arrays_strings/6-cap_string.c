139c1b550560f8422b1afe5d7203a7b7f7bdd723
Your Name <you@example.com>
2023-10-06 09:19:48 -0700

    have it search previous ous character [i - 1] to check if its a line breaking character

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -11,7 +11,13 @@ char *cap_string(char *str)
 
 	for (i = 0; str[i] != '\0'; i++)
 	{
-		if ((i == 0 || str[i - 1] == ' ' || str[i - 1] == '\t' || str[i - 1] == '\n' || str[i - 1] == ',' || str[i - 1] == ';' || str[i - 1] == '.' || str[i - 1] == '!' || str[i - 1] == '?' || str[i - 1] == '"' || str[i - 1] == '(' || str[i - 1] == ')' || str[i - 1] == '{' || str[i - 1] == '}') && (str[i] >= 'a' && str[i] <= 'z'))
+		if ((i == 0 || str[i - 1] == ' ' || str[i - 1] == '\t' ||
+	str[i - 1] == '\n' || str[i - 1] == ',' ||
+	str[i - 1] == ';' || str[i - 1] == '.' ||
+	str[i - 1] == '!' || str[i - 1] == '?' ||
+	str[i - 1] == '"' || str[i - 1] == '(' ||
+	str[i - 1] == ')' || str[i - 1] == '{' ||
+	str[i - 1] == '}') && (str[i] >= 'a' && str[i] <= 'z'))
 		{
 			str[i] = str[i] - 'a' + 'A';
 		}
