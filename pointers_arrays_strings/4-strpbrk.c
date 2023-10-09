03bbb9e4c3b0d83a374f01a1b2213b0f8a2db446
Your Name <you@example.com>
2023-10-09 09:05:30 -0700

    compares characters beters between string and acceptable characters and returns pointer to first match

---
+++ pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * _strpbrk - will search a string for any of a set of bytes
+ * @s: the string to be searched
+ * @accept: acceptable bytes
+ *
+ * Return: pointer to the byte in s that matches or NULL if not found
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	while (*s != '\0')
+	{
+		char *accept_ptr = accept;
+
+		while (@accept_ptr != '\0')
+		{
+			if (*s == *accept_ptr)
+			{
+				return (s);
+			}
+			accept_ptr++;
+		}
+		s++;
+	}
+
+	return (NULL);
+}
