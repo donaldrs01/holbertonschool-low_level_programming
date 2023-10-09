40b2e15de875a82e0d68bb694373adc2f64e563f
Your Name <you@example.com>
2023-10-09 09:40:46 -0700

    i < 8, j < 8, iterates through and prints %c for each point in [i,j]

---
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_chessboard - prints a chessboard
+ * @a: the array in which each element is printed
+ *
+ * Return: 0
+ */
+void print_chessboard(char (*a)[8])
+{
+	int i;
+	int j;
+
+	for (int i = 0; i < 8; i++)
+	{
+		for (int j = 0; j < 8; j++)
+		{
+			printf("%c", a[i][j]);
+		}
+		printf("\n");
+	}
+}
