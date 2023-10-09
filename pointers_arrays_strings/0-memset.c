8fd803e5f604184288041e1c49f6c04e8310e384
Your Name <you@example.com>
2023-10-09 08:20:50 -0700

    assigns constant byte to each byte pointed to by s

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _memset - will fill the first 'n' bytes of memory area
+ * @s: a pointer to the area of memory to be filled
+ * @b: constant byte v
+ * @n: the number of bytes of memory to be filled
+ *
+ * Return: a pointer to memory area 's'
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	char *ptr = s;
+	
+	while (n--)
+	{
+		*ptr++ = b;
+	}
+
+	return (s);
+}
