ca9d9a8f10bd3965ee9da1880cdb20d385ba9c8c
Your Name <you@example.com>
2023-10-11 12:46:13 -0700

    using recursion to print in reverse

---
+++ recursion/1-print_rev_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _print_rev_recursion - will print a string in reverse
+ * @s: the string to be printed
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_print_rev_recursion(s + 1);
+	}
+
+	putchar(*s);
+	/**
+	 * This works because of the stack.
+	 * When string reaches end, it prints back in reverse order.
+	 * Top of the stack is the most recent character (at end of string).
+	 */
+}
