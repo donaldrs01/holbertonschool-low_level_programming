b92dac56b6d0c0e372f99f517360bd6d6d4b03b8
Your Name <you@example.com>
2023-10-11 12:14:47 -0700

    using _puts_recursion(s + 1) to iterate to iterate through string

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _puts_recursion - will use recursion to print a string
+ * @s: the string that we will print
+ */
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		putchar('\n');
+		return;
+	}
+
+	putchar(*s);
+	_puts_recursion(s + 1);
+}
