32b086f911bfb4a4006d3ad83c44e811f6f870f1
Your Name <you@example.com>
2023-10-11 14:09:16 -0700

    recursion to do exponents

---
+++ recursion/4-pow_recursion.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _pow_recursion - will return value of x to the power of y
+ * @x: the integer 
+ * @y: the power at which the integer will be raised 
+ *
+ * Return: -1 if y < 0, otherwise correct value
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	else if (y == 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return (x * _pow_recursion(x, y - 1));
+	}
+}
