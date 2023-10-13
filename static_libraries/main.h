0e54b99f408bf80871ca9f246dc75c430329ede8
Your Name <you@example.com>
2023-10-13 12:10:35 -0700

    blah

--- static_libraries/main.h
+++ static_libraries/main.h
@@ -38,7 +38,7 @@ int _atoi(char *s);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
-int _strcmp (char *s1, char *s2);
+int _strcmp(char *s1, char *s2);
 void reverse_array(int *a, int n);
 char *string_toupper(char *str);
 char *cap_string(char *str);
@@ -58,4 +58,5 @@ int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int is_prime_number(int n, int i);
-#endif 
+#endif
+
