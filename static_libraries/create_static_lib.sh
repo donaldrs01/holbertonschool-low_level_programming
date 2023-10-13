9ef0c242ebd3cdd136725e573f6f961369b1c212
Your Name <you@example.com>
2023-10-13 12:23:31 -0700

    created script to compile files into static library

---
+++ static_libraries/create_static_lib.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+c_files=$(ls *.c 2>dev/null)
+
+if [ -z "$c_files" ]; then
+	echo "No .c files found in the current directory."
+	exit 1
+fi
+
+for file in $c_files; do
+	object_file="${file%.c}.o"
+	gcc -c "$file" -o "$object_file"
+done
+
+ar rcs liball.a *.o
+
+rm -f *.o
+
+echo "Static library liball.a has been created."
