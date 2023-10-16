69e32b093e960aee74c96a3cb9147cbd9591825e
Your Name <you@example.com>
2023-10-16 10:02:28 -0700

    printf argv[%s], argv[0] to print progra m name

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,13 +1,13 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * main - will print the name of the program (contained in first index spot) 
+ * main - will print the name of the program (contained in first index spot)
  * @argc: the number of arguments
  * @argv: the names of the arguments
  *
  * Return: 0 on success
  */
-int main (int argc, char* argv[])
+int main(int argc, char *argv[])
 {
 	(void)argc;
 
