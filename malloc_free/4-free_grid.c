afc2eef9f6042f9cd99418f950ed68551a9f9059
Your Name <you@example.com>
2023-10-19 12:57:57 -0700

    iterate over each row of grid, then free grid itself

---
+++ malloc_free/4-free_grid.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+#include <stdlib.h>
+/**
+ * free_grid - will frees the grid previously created
+ * @grid: the pointer to the pointer of integers
+ * @height: the height of the grid
+ */
+void free_grid(int **grid, int height);
+{
+	int i;
+
+	if (grid == NULL)
+	{
+		return;
+	}
+
+	for (i = 0; i < height; i++)
+	{
+		free(grid[i]);
+	}
+
+	free(grid);
+}
