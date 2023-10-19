c10a05578be37eccf65829717a71632c87fd9fbe
Your Name <you@example.com>
2023-10-19 10:09:58 -0700

    malloc strlen(str) + 1

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -1,6 +1,7 @@
 #include "main.h"
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 /**
  * _strdup - returns pointer to new space in memory contains copy of input
  * @str: string to be copied into memory
