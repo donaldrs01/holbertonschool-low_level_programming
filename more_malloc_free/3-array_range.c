c1da14ebc34a7e29a71f1f39fd249e4d9f020157
Your Name <you@example.com>
2023-10-20 11:30:43 -0700

    max - min to calculate array size, then fill array with values from min to max by saying array[i] = min + i

---
+++ more_malloc_free/3-array_range.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+/**
+ * array_range - creates an array of integers with a min and max value
+ * @min: the minimum value
+ * @max: the maximum value
+ *
+ * Return: pointer to newly created array, NULL under certain conditions
+ */
+int *array_range(int min, int max)
+{
+	int size;
+	int *array;
+	int i;
+
+	if (min > max)
+	{
+		return (NULL);
+	}
+
+	size = max - min + 1;
+
+	*array = malloc(size * sizeof(int));
+	if (array == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < size; i++)
+	{
+		array[i] = min + i;
+	}
+
+	return (array);
+}
