b57cc3da58c39fc64bf05657239f68cf7b94db59
Your Name <you@example.com>
2023-10-20 12:39:04 -0700

    using _FILE_ macro

---
+++ preprocessor/2-main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - will print the name of the file the function was compiled from
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	printf("%s\n", _FILE_);
+	return (0);
+}
