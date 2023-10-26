1e848f74d3870933569255282392934e5430b559
Your Name <you@example.com>
2023-10-26 14:37:31 -0700

    printing string by passing it to the function pointer f

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,10 +1,14 @@
 #include "function_pointers.h"
 /**
- * print_name - will print the name string 
+ * print_name - will print the name string
  * @name: a string that we want to print
  * @f: a function pointer that points to a function taking a char * argument
  */
 void print_name(char *name, void (*f)(char *))
 {
+	if (f == NULL)
+	{
+		return;
+	}
 	f(name);
 }
