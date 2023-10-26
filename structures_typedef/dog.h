909057bc872ba335249e1ca6bedb0ab32c3d8d8d
Your Name <you@example.com>
2023-10-26 09:36:38 -0700

    creating 'dog' structure with proper betty documentation

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,6 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a basic structure with three variables
+ * @name: the name of the dog
+ * @age: the age of the dog
+ * @owner: the owner of the dog
+ *
+ * Description: I don't know what else to include here
+ */
+
 struct dog
 {
-	char* name;
+	char *name;
 	float age;
-	char* owner;
+	char *owner;
 };
+
+#endif
