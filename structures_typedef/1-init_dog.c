d4142206ea58eea545253b1227bfc42422f030b3
Your Name <you@example.com>
2023-10-26 09:45:39 -0700

    takes pointer to a struct dog as argument, use strcpy to copy name and owner strings and assign age directly

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+#include "main.h"
+#include <string.h>
+/**
+ * init_dog - initializes variable of type struct dog
+ * @d: the initialized variable
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	strcpy(d->name, name);
+	d->age = age;
+	strcpy(d->owner, owner);
+}
