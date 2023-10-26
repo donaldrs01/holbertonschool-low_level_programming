a467ab67c899bdc1d51b419012e4a49e17af046c
Your Name <you@example.com>
2023-10-26 13:16:22 -0700

    creates new_dog structure that takes in name/age/owner, allocates memory for strings, and copies the arguments into memory

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -13,12 +13,14 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *new_dog = malloc(sizeof(dog_t));
+
 	if (new_dog == NULL)
 	{
 		return (NULL);
 	}
 
 	new_dog->name = malloc(strlen(name) + 1);
+
 	if (new_dog->name == NULL)
 	{
 		free(new_dog);
@@ -26,6 +28,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 	}
 
 	new_dog->owner = malloc(strlen(owner) + 1);
+
 	if (new_dog->owner == NULL)
 	{
 		free(new_dog->name);
