93316e73e41107dd8ac10040a79173986ca57013
Your Name <you@example.com>
2023-10-26 15:22:26 -0700

    iterates through an array, calling cmp function to compare values

---
+++ function_pointers/2-int_index.c
@@ -0,0 +1,32 @@
+#include "function_pointers.h"
+#include <stdio.h>
+/**
+ * int_index - searches for an integer
+ * It will return the index of the first element for which cmp function
+ * does not return 0
+ * @array: integer array
+ * @size: number of elements in the array
+ * @cmp: pointer to the function used to compare values
+ *
+ * Return: -1 when no match or size <= 0, otherwise index value
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (size <= 0)
+	{
+		return (-1);
+	}
+
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]) != 0)
+		{
+			return (i);
+		}
+	}
+
+	return (-1);
+}
+
