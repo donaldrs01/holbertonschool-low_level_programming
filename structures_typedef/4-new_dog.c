f08229154c5ec67f8cf102861810e58606bf780d
Your Name <you@example.com>
2023-10-26 13:14:12 -0700

    creates new_dog structure that takes in name/age/owner, allocates memory for strings, and copies the arguments into memory

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -7,6 +7,8 @@
  * @name: name of dog
  * @age: age of dog
  * @owner: owner of dog
+ *
+ * Return: pointer to newly created dog_t structure
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
