e1153847c2757000d59869269176618e921e473e
Your Name <you@example.com>
2023-10-26 14:48:54 -0700

    iterates through array and for each element, calls the function pointer to by action and passes the element as an argument

---
+++ function_pointers/1-array_iterator.c
@@ -0,0 +1,14 @@
+#include "function_pointers.h"
+/**
+ * array_iterator - executes function given as argument on each element of array
+ * @array: a pointer to the array of integers
+ * @size: the size of the array
+ * @action: function pointer that points to a function taking an int argument
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	for (size_t i = 0; i < size; i++)
+	{
+		action(array[i]);
+	}
+}
