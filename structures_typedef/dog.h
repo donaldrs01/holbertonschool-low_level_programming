8b2d524b9faa4672935effd688598d03f6a535e5
Your Name <you@example.com>
2023-10-26 12:45:17 -0700

    typedef to change type name to dog_t

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -10,12 +10,12 @@
  * Description: I don't know what else to include here
  */
 
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+} dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 
