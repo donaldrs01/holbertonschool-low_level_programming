67ef6487701f37c1ff4036d0aa4c520aa852c79d
Your Name <you@example.com>
2023-10-27 09:48:04 -0700

    this was a doozy

---
+++ function_pointers/3-main.c
@@ -0,0 +1,41 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+	int num1;
+	char *operator;
+	int num2;
+	int result;
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		return (98);
+	}
+
+	num1 = atoi(argv[1]);
+	operator = argv[2];
+	num2 = atoi(argv[3]);
+
+	int (*op_func)(int, int) = get_op_func(operator);
+
+	if (op_func == NULL)
+	{
+		printf("Error\n");
+		return (99);
+	}
+
+	if ((strcmp(operator, "/") == 0 | strcmp(operator, "%") == 0) && num2 == 0)
+	{
+		printf("Error\n");
+		return (100);
+	}
+
+	result = op_func(num1, num2);
+	printf("%d\n", result);
+
+	return (0);
+}
+
---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,48 @@
+#include "3-calc.h"
+/**
+ * op_add - returns the sum of ints a and b
+ * @a: the first integer
+ * @b: the second integer 
+ *
+ * Return: the sum of a and b
+ */
+int op_add(int a, intb)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - returns the difference of a and b
+ * @a: the first integer
+ * @b: the second integer 
+ *
+ * Return: the difference of a and b
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_div - returns the result of the division of a by b
+ * @a: the first integer
+ * @b: the second integer
+ *
+ * Return: the result of the division of a by b
+ */
+int op_div(int a, intb)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - returns the remainder of the division of a by b
+ * @a: the first integer
+ * @b: the second integer
+ *
+ * Return: the remainder of the division of a by b
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
