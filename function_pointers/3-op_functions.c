a0e445f8111cf765bb8bfc4a3b04e84aad3ae595
Your Name <you@example.com>
2023-10-27 09:55:07 -0700

    phew

--- function_pointers/3-op_functions.c
+++ function_pointers/3-op_functions.c
@@ -6,7 +6,7 @@
  *
  * Return: the sum of a and b
  */
-int op_add(int a, intb)
+int op_add(int a, int b)
 {
 	return (a + b);
 }
@@ -30,7 +30,7 @@ int op_sub(int a, int b)
  *
  * Return: the result of the division of a by b
  */
-int op_div(int a, intb)
+int op_div(int a, int b)
 {
 	return (a / b);
 }
