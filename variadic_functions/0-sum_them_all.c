bd6d2e1d9f64a9cb787d7ce2d7c5b32bcfd0e799
Your Name <you@example.com>
2023-10-30 11:04:41 -0700

    using va_start and va_end with variable args to add sums together passed as arguments

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -12,6 +12,7 @@ int sum_them_all(const unsigned int n, ...)
 	int sum;
 	va_list args; /* holds info about variable arguments */
 	int num;
+	unsigned int i;
 
 	if (n == 0)
 	{
@@ -21,7 +22,7 @@ int sum_them_all(const unsigned int n, ...)
 	sum = 0;
 	va_start(args, n);
 
-	for (unsigned int i = 0; i < n; i++)
+	for (i = 0; i < n; i++)
 	{
 		num = va_arg(args, int);
 		sum += num;
