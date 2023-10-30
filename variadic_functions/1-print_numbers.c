0dac9ced558896a30a629d426aec4cda1b6d186a
Your Name <you@example.com>
2023-10-30 11:32:38 -0700

    variable arguments of numbers to print separated by certain character

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -14,8 +14,9 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	unsigned int i;
 	int num;
 	va_list args;
+
 	va_start(args, n);
-	
+
 	for (i = 0; i < n; i++)
 	{
 		num = va_arg(args, int);
