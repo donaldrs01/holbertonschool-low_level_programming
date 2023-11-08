5990d40c09e32754450fa8eed626fc2987d4baa2
Your Name <you@example.com>
2023-11-08 14:45:18 -0800

    first task - print elements of list_t

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -1,7 +1,7 @@
 #ifndef LISTS_H
 #define LISTS_H
 /**
- * struct list_t - sample data structure
+ * struct list_s - sample data structure
  * @str: string pointer
  * @len: length of the string
  * @next: points to the next node
@@ -14,7 +14,7 @@ typedef struct list_s /* singly linked list node structure */
 	char *str;
 	unsigned int len;
 	struct list_s *next;
-} list_t;
+}list_t;
 
 int _putchar(char c);
 size_t print_list(const list_t *h);
