e269410cfb22787391ee4a8c9eec842561a2529e
Your Name <you@example.com>
2023-11-08 15:36:18 -0800

    second task - return the number of elements by creating a current variable and syncing it with the count as we move from node to node

--- singly_linked_lists/1-list_len.c
+++ singly_linked_lists/1-list_len.c
@@ -1,5 +1,7 @@
 #include "lists.h"
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 /**
  * list_len - function that returns # of elements in linked list_t list
  * @h: pointer to object of type list_t
@@ -8,3 +10,14 @@
  */
 size_t list_len(const list_t *h)
 {
+	size_t count = 0;
+	const list_t *current = h;
+
+	while (current != NULL)
+	{
+		count++; /* increment count for each element in list */
+		current = current->next; /* move to the next node */
+	}
+
+	return (count);
+}
