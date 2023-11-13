4efa8fb3c0c0bd64eb761a4d380ae55e80f2a43d
Your Name <you@example.com>
2023-11-13 13:13:55 -0800

    left-shift 1 by index, then compares this number to each bit of n with OR operation

---
+++ bit_manipulation/3-set_bit.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * set_bit - sets the value of a bit to '1' at given index
+ * @n: pointer to number in which bit is set
+ * @index: position of the bit to set in number 'n'
+ *
+ * Return: 1 on success, -1 on error
+ */
+int set_bit(unsigned long int *n, unsigned int index);
+{
+	if (index >= sizeof(unsigned long it) * 8 - 1)
+	{
+		return (-1);
+	}
+
+	*n = *n | (1 << index);
+	return (1);
+	/**
+	 * left-shifts 1 by 'index' positions 
+	 * creates binary number where all bits are 0
+	 * EXCEPT for index position, where it is 1
+	 * after doing this, does bitwise OR operation 
+	 * compares each bit of n to # created by shift
+	 * if either # is 1, resulting bit is 1
+	 * sets the bit at index position to 1
+	 */
+}
