64b897a081fb0dcd1fa73f7e43edc7abc6c84b77
Your Name <you@example.com>
2023-11-13 13:15:49 -0800

    left-shift 1 by index, then compares this number to each bit of n with OR operation

--- bit_manipulation/3-set_bit.c
+++ bit_manipulation/3-set_bit.c
@@ -9,7 +9,7 @@
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	if (index >= sizeof(unsigned long it) * 8 - 1)
+	if (index >= sizeof(unsigned long int) * 8 - 1)
 	{
 		return (-1);
 	}
