c48a1435bb9ae8e1e9a69f50f04869c93584b58d
Your Name <you@example.com>
2023-11-18 08:14:25 -0800

    adds new node, move to end of list, add new node and correctly set next and prev pointers

---
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -0,0 +1,45 @@
+#include "lists.h"
+#include <stdarg.h>
+#include <stddef.h>
+#include <stdio.h>
+/**
+ * add_dnodeint_end - adds a new node at end of linked list
+ * @head: double pointer to head of the list
+ * @n: value to be stored in new node
+ *
+ * Return: address of the new element, or NULL if failed
+ */
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+	dlistint_t *new_node;
+	dlistint_t *current;
+	dlistint_t *last = *head;
+
+	new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
+	{
+		return (NULL);
+	}
+
+	new_node->n = n;
+	new_node->next = NULL; /* next node NULL because end of list */
+
+	if (*head == NULL) /* if list empty, new_node is head */
+	{
+		new_node->prev = NULL;
+		*head = new_node;
+	}
+	else /* traverse to end of list */
+	{
+		dlistint_t *current = *head;
+
+		while (current->next != NULL)
+		{
+			current = current->next;
+		}
+
+		current->next = new_node; /* add newnode at end of list */
+		new_node->prev = current; /* prev pointer to current */
+	}
+
+	return (new_node);
