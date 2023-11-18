49636a5f9da5fed7092dca994e37e1c7b80f3d29
Your Name <you@example.com>
2023-11-18 07:46:50 -0800

    mallocs space, then adds new_node and places it at front of list

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -1,6 +1,7 @@
 #include "lists.h"
 #include <stddef.h>
 #include <stdio.h>
+#include <stdlib.h>
 /**
  * add_dnodeint - function that adds a new node at head of list
  * @head: double pointer to head of list
