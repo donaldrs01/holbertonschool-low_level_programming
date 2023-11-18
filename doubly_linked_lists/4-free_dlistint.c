1402835b52c7173a2f1bd41064a5d00677f74c66
Your Name <you@example.com>
2023-11-18 08:56:41 -0800

    save address of next node in next_node, then free current before moving to next node

---
+++ doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+#include <stdarg.h>
+#include <stddef.h>
+#include <stdio.h>
+/**
+ * free_dlistint - function that frees a dlistint_t list
+ * @head: pointer to head of list
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *current = head; /* curernt initialized to head of list */
+	dlistint_t *next_node;
+
+	while (current != NULL)
+	{
+		next_node = current->next; /* address of next node saved in next_node */
+		free(current);
+		current = next_node;
+	}
+}
