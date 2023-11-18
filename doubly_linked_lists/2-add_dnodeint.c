54f51a0b2179886c5bf1c16fcce828071f22ac67
Your Name <you@example.com>
2023-11-18 07:45:48 -0800

    mallocs space, then adds new_node and places it at front of list

---
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,35 @@
+#include "lists.h"
+#include <stddef.h>
+#include <stdio.h>
+/**
+ * add_dnodeint - function that adds a new node at head of list
+ * @head: double pointer to head of list
+ * @n: integer value of the new node
+ *
+ * Return: address of the new node, or NULL if failed
+ */
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *new_node;
+
+	new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
+	{
+		return (NULL); /* return NULL if malloc fails */
+	}
+
+	new_node->n = n; /* sets value of new node */
+	new_node->prev = NULL; /* NULL precedes head node */
+	new_node->next = *head; /* current head comes after new node */
+
+	if (*head != NULL)
+	{
+		(*head)->prev = new_node; /**
+					    * prev pointer of old head points to new_node
+					    */
+	}
+	*head = new_node; /* updates head pointer to point to new_node */
+
+	return (new_node);
+}
+
