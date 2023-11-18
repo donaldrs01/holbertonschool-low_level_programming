fbb2763ae2f9da8fa85c09cab8bdf791f2d9b3bf
Your Name <you@example.com>
2023-11-18 09:11:41 -0800

    initialize sum to 0, then move current pointer through list. add data from each node to sum, return sum

---
+++ doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+#include <stdarg.h>
+#include <stdio.h>
+#include <stddef.h>
+/**
+ * sum_dlistint - function that returns the sum of all data in the linked list
+ * @head: pointer to head of list
+ *
+ * Return: sum of all data (n), 0 if list is empty
+ */
+int sum_dlistint(dlistint_t *head)
+{
+	dlistint_t *current = head;
+	int sum = 0;
+
+	while (current != NULL)
+	{
+		sum =+ current->n; /* adds data (n) of current node to 'sum' variable */
+		current - current->next; /* move current to next node */
+	}
+
+	return (sum);
+}
