c328545bab9593f1e92d2fbbe9fa91f76c4185bf
Your Name <you@example.com>
2023-11-18 09:42:31 -0800

    insert new_node after given index

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -14,7 +14,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	dlistint_t *new_node;
 	dlistint_t *current;
-	int i;
+	unsigned int i;
 
 	new_node = malloc(sizeof(dlistint_t));
 	if (new_node == NULL)
