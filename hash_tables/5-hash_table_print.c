1d7aef46a788e303ac6d33c81268ef806631e9fa
Your Name <you@example.com>
2023-11-20 11:12:33 -0800

    i iterates through array, prints key/value at each index

--- hash_tables/5-hash_table_print.c
+++ hash_tables/5-hash_table_print.c
@@ -19,7 +19,7 @@ void hash_table_print(const hash_table_t *ht)
 		current = ht->array[i];
 		while (current != NULL)
 		{
-			printf("'%s': '%s'", i, current->key, current->value);
+			printf("'%s': '%s'", current->key, current->value);
 			current = current->next;
 		}
 	}
