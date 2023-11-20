1ccbdbe2b0bee3b58ac128c47c865b112d9ccdc3
Your Name <you@example.com>
2023-11-20 11:17:38 -0800

    i iterates through array, prints key/value at each index

--- hash_tables/5-hash_table_print.c
+++ hash_tables/5-hash_table_print.c
@@ -8,19 +8,29 @@ void hash_table_print(const hash_table_t *ht)
 {
 	unsigned long int i;
 	hash_node_t *current;
+	int is_first_pair = 1l /* flag to check if first pair for printing purposes */
 
 	if (ht == NULL)
 	{
 		return;
 	}
 
+	printf("{"); /* print opening brace */
+
 	for (i = 0; i < ht->size; i++)
 	{
 		current = ht->array[i];
 		while (current != NULL)
 		{
+			if (!is_first_pair)
+			{
+				printf(", ");
+			}
 			printf("'%s': '%s'", current->key, current->value);
+			is_first_pair = 0;
 			current = current->next;
 		}
 	}
+
+	printf("}");
 }
