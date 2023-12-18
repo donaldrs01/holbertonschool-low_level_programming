fb784056f270ed13c079eeb9f68dbf3c387eff51
Your Name <you@example.com>
2023-12-18 12:46:44 -0800

    fixed formatting

--- search_algorithms/1-binary.c
+++ search_algorithms/1-binary.c
@@ -26,7 +26,7 @@ int binary_search(int *array, size_t size, int value)
 
 	while (left <= right)
 	{
-		printf("Searching array: ");
+		printf("Searching in array: ");
 		for (i = left; i <= right; i++)
 		{
 			printf("%d", array[i]);
