6d2c19ce250d1073abc01f9bc09f6b11f43b4380
Your Name <you@example.com>
2023-12-18 12:06:43 -0800

    fixed print statement

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -25,7 +25,7 @@ int linear_search(int *array, size_t size, int value)
 
 		if (array[i] == value)
 		{
-			printf("Found %d at index: %lu\n", value, i);
+			printf("Found at index: %lu\n", i);
 			return (i); /* return index if value found */
 		}
 	}
